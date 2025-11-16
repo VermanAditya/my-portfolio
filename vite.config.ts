8393b2fddb72da61fc45e50d60c14dd4dfe0816f
Aditya <adiitya821@gmail.com>
2025-11-17 02:41:18 +0530

    Added Vite config + markdown loader + fixed tsconfig

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mdPlugin from "vite-plugin-markdown";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    mdPlugin()
+  ]
+});
